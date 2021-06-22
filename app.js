75e15386cecd028441c901e78c41793e5416ae5f
Lucia <luciareami11@gmail.com>
2021-06-21 22:07:15 -0300

    arreglando viws

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.listen(process.env.PORT ||"1000", function(){
     console.log("Servidor corriendo en el puerto 1000")
 });
     app.get("/", (req, res) => {
-        res.sendFile(path.resolve(__dirname, ".views/home.html")) 
+        res.sendFile(path.resolve(__dirname, "./views/home.html")) 
     })
     app.get('/register', (req, res) => {
         res.sendFile(path.resolve(__dirname, './views/register.html'))
