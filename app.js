932419e6bda24917b6a7fb09df8f0de0ca904237
Lucia <luciareami11@gmail.com>
2021-06-21 22:03:28 -0300

    mensaje

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require ("path")
+const app = express ();
+const publicPath = path.resolve(__dirname, "./public") 
+app.use( express.static(publicPath))
+app.listen(process.env.PORT ||"1000", function(){
+    console.log("Servidor corriendo en el puerto 1000")
+});
+    app.get("/", (req, res) => {
+        res.sendFile(path.resolve(__dirname, ".views/home.html")) 
+    })
+    app.get('/register', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './views/register.html'))
+    });
+    
+    app.get('/login', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './views/login.html'))
+    })
